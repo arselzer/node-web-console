d9e3c4eccd06a16c2606a2302dac3f7d34ca61b4
Alexander Selzer <aselzer3@gmail.com>
2014-03-26 10:27:52 +0100

    Works.

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+var vm = require("vm");
+var util = require("util");
+
 var io = require("socket.io").listen(8767);
+
+function vmConsole() {
+  this.output = "";
+}
+
+vmConsole.prototype.log = function() {
+  this.output += util.format.apply(this, arguments) + "\n";
+}
 
 io.sockets.on("connection", function(socket) {
   socket.on("js", function(data) {
-    console.log("js", data);
+    console.log("Received:", data);
+    
+    var context = vm.createContext({
+    	"console": new vmConsole,
+    });
+    
+    vm.runInContext(data, context);
+    
+    socket.emit("output", context.console.output);
   });
 });
--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,20 @@ var hostname = "localhost";
 var socket = io.connect("http://" + hostname + ":8767");
 
 var jsInput = document.getElementById("jsinput");
+var jsOutput = document.getElementById("jsoutput");
+var submitJs = document.getElementById("submitjs");
 
-jsinput.addEventListener("keypress", function(e) {
-  if (e.keyCode === 13) {
-    socket.emit("js", jsInput.value);
-  }
+submitJs.addEventListener("focus", function() {
+  setTimeout(function() {
+    submitJs.blur();
+  }, 400);
+});
+
+submitJs.addEventListener("click", function(e) {
+	var js = editor.getSession().getValue();
+  socket.emit("js", js);
+});
+
+socket.on("output", function(data) {
+  jsOutput.innerHTML = data;
 });
